f2eea7d0e0e1507edc6203d2f86ac1179a6e8eba
Rafael Saca <raffaellsaca@gmail.com>
2024-01-18 21:41:28 -0300

    feat: Formatação do preço

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,3 +1,4 @@
+import { currencyFormat } from '../../helpers/currencyformat'
 import { Container } from './styles'
 import {FiPlus} from 'react-icons/fi'
 
@@ -14,7 +15,7 @@ export function Snacks({ snacks }: SnacksProps) {
           <img src={snack.image} alt={snack.name} />
           <p>{snack.description}</p>
           <div>
-            <strong>{snack.price}</strong>
+            <strong>{currencyFormat(snack.price)}</strong>
             <button type='button'><FiPlus/></button>
           </div>
         </div>
