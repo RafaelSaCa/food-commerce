e99928ba98c720cf596ec2b501c8276e65e9e549
Rafael Saca <raffaellsaca@gmail.com>
2024-01-19 21:58:12 -0300

    feat: Recuperando dados Drinks do json-server

--- src/pages/Burgers/index.tsx
+++ src/pages/Burgers/index.tsx
@@ -1,41 +1,25 @@
 import { Head } from '../../components/Head'
 import { SnackTitle } from '../../components/SnackTitle'
-import { Snacks } from '../../components/Snacks';
+import { Snacks } from '../../components/Snacks'
+import { useState, useEffect } from 'react'
+import { getBurgers } from '../../services/api'
 
 export default function Burgers() {
-  const data = [
-    {
-      id: 1,
-      snack: 'burger',
-      name: 'Mega',
-      description: 'Tamanho Família ',
-      price: 25.5,
-      image: 'https://i.imgur.com/upjIUnG.jpg ',
-    },
+    const [burgers, setBurgers] = useState([])
 
-    {
-      id: 2,
-      snack: 'burger',
-      name: 'Extra Bacon',
-      description: 'Muito Bacon!!',
-      price: 25.5,
-      image: 'https://i.imgur.com/B4J04AJ.jpg',
-    },
-    {
-      id: 3,
-      snack: 'burger',
-      name: 'Salada',
-      description: 'Alface Tomate e Queijo',
-      price: 20.0,
-      image: 'https://i.imgur.com/0umadnY.jpeg',
-    }
-  ]
+    useEffect( ( ) => {
+      ( async () => {
+        const burgerRequest = await getBurgers()
 
-  return(
-  <>
-    <Head title='Hambúrgueres' />
-   <SnackTitle>Hambúrgueres</SnackTitle>
-   <Snacks snacks={data}></Snacks>
-  </>
+        setBurgers(burgerRequest.data)
+      })()
+    }, [])
+
+  return (
+    <>
+      <Head title='Hambúrgueres' />
+      <SnackTitle>Hambúrgueres</SnackTitle>
+      <Snacks snacks={burgers}></Snacks>
+    </>
   )
 }
