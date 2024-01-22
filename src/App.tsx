0043749c350e5c54b275e38e3afe89960944067f
Rafael Saca <raffaellsaca@gmail.com>
2024-01-21 22:17:06 -0300

    feat: Movendo a lógica de Burger para Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,46 @@
+import { createContext, useEffect, useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-
 import { AppRoutes } from './routes'
 
+
+
+import { Normalize } from 'styled-normalize'
+import { SnackData } from './interfaces/SnackData'
+import { getBurgers } from './services/api'
 import { Theme } from './styles/Theme'
 import { GlobalStyle } from './styles/global'
-import { Normalize } from 'styled-normalize'
+
+interface SnackContextProps {
+  burgers: SnackData[]
+ // pizzas: SnackData[]
+//  drinks: SnackData[]
+//  iceCreams: SnackData[]
+}
+
+export const SnackContext = createContext( { } as SnackContextProps)
 
 export default function App() {
+  const [burgers, setBurgers] = useState<SnackData[]>([])
+
+  useEffect(() => {
+    ;(async () => {
+      const burgerRequest = await getBurgers()
+
+      setBurgers(burgerRequest.data)
+    })()
+  }, [])
+
   return (
     <BrowserRouter>
       <Theme>
+        <SnackContext.Provider value={ { burgers }}>
         <AppRoutes />
         <GlobalStyle />
         <Normalize />
+        </SnackContext.Provider>
       </Theme>
     </BrowserRouter>
+
   )
-}
+
+  }
--- src/pages/Burgers/index.tsx
+++ src/pages/Burgers/index.tsx
@@ -1,20 +1,11 @@
+import { useContext } from 'react'
+import { SnackContext } from '../../App'
 import { Head } from '../../components/Head'
 import { SnackTitle } from '../../components/SnackTitle'
 import { Snacks } from '../../components/Snacks'
-import { useState, useEffect } from 'react'
-import { getBurgers } from '../../services/api'
-import { SnackData } from '../../interfaces/SnackData'
 
 export default function Burgers() {
-  const [burgers, setBurgers] = useState<SnackData[]>([])
-
-  useEffect(() => {
-    ;(async () => {
-      const burgerRequest = await getBurgers()
-
-      setBurgers(burgerRequest.data)
-    })()
-  }, [])
+   const {burgers } = useContext(SnackContext)
 
   return (
     <>
