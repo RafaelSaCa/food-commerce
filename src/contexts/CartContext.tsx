cee8735b7e950c786326ba60cc44431ee1f63b10
Rafael Saca <raffaellsaca@gmail.com>
2024-01-22 21:35:40 -0300

    feat: Criando o Context de carrinho de compras

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,36 @@
+import { ReactNode, createContext } from 'react'
+import { SnackData } from '../interfaces/SnackData'
+
+interface Snack extends SnackData{
+  quantity: number
+  subtotal: number
+}
+
+interface UpdateCartProps {
+  id: number
+  snack: string
+  newQuantity: number
+}
+
+interface RemoveSnackFromCart {
+  id: number
+  snack: string
+}
+
+interface CartContextProps {
+  cart: Snack[ ]
+  addSnackIntoCart: ( snack: SnackData) => void
+  removeSnackFromCart: ({id, snack }: RemoveSnackFromCart) => void
+  updateCart: ( {id, snack, newQuantity }: UpdateCartProps) => void
+
+}
+
+interface CartProviderProps {
+  children: ReactNode
+}
+
+export const CartContext = createContext({ } as CartContextProps)
+
+export function CartProvider({ children}: CartProviderProps) {
+  //
+}
