418aa73494cc84f0b017e0f58f2a0952d60f0586
Rafael Saca <raffaellsaca@gmail.com>
2024-01-22 22:25:49 -0300

    feat: Atualizando a quantidade no carrinho

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -35,10 +35,29 @@ export function CartProvider({ children}: CartProviderProps) {
     const [ cart, setCart] = useState<Snack[]>([])
 
     function addSnackIntoCart ( snack: SnackData): void{
+        const snackExistentInCart = cart.find( (item) => item.snack === snack.snack
+                                                      && item.id === snack.id)
+
+    if (snackExistentInCart){
+      const newCart = cart.map( (item) => {
+        if (item.id === snack.id){
+          const quantity = item.quantity + 1
+          const subtotal = item.price * quantity
+
+          return { ...item, quantity, subtotal }
+         }
+         return item
+      })
+      console.log(`newCart atualização`, newCart)
+      setCart(newCart)
+
+      return
+    }
+
       const newSnack = {...snack, quantity:1, subtotal: snack.price }
       const newCart = [...cart, newSnack]
 
-      console.log(`newCart`, newCart)
+      console.log(`newCart adição`, newCart)
       setCart(newCart)
     }
     return(
