c912dfa309fb6e019e523522b4af6125025d3603
Rafael Saca <raffaellsaca@gmail.com>
2024-01-31 20:47:55 -0300

    Organização diretório de páginas

--- src/pages/Main/IceCreams/index.tsx
+++ src/pages/Main/IceCreams/index.tsx
@@ -1,7 +1,7 @@
-import { Head } from '../../components/Head'
-import { SnackTitle } from '../../components/SnackTitle'
-import { Snacks } from '../../components/Snacks'
-import { useSnack } from '../../hooks/useSnack'
+import { Head } from '../../../components/Head'
+import { SnackTitle } from '../../../components/SnackTitle'
+import { Snacks } from '../../../components/Snacks'
+import { useSnack } from '../../../hooks/useSnack'
 
 export default function IceCreams ( ){
   const {iceCreams } = useSnack()
